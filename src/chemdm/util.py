e77d1f7d51fe589be003b10dc9645cdd93918ac4
Hannes Vandecasteele <hannesvdc@gmail.com>
2026-03-16 13:00:03 -0400

    Implemented general module to associate chemical information to each atom in the molecule.

--- src/chemdm/util.py
+++ src/chemdm/util.py
@@ -7,3 +7,26 @@ def getGradientNorm( model : pt.nn.Module ):
             grads.append(param.grad.view(-1))
     grads = pt.cat(grads)
     return pt.norm(grads).item()
+
+@pt.no_grad()
+def isInteger( x : pt.Tensor,
+              float_tol : float = 1e-7 ) -> pt.Tensor:
+    """
+    Check if the entries of the input tensor are integers. Returns a bool tensor
+    of the same size indicating whether the corresponding entry in x is an integer.
+
+    Arguments
+    ---------
+    x: pt.Tensor (any size)
+        The array to check.
+    float_tol : float
+        Tolerance used to check floating point numbers. Default 1e-7 for single precision.
+
+    Returns
+    -------
+    is_integer : pt.Tensor of type bool
+        The boolean output tensor.
+    """
+    if x.dtype in [pt.uint8, pt.int8, pt.int16, pt.int32, pt.int64]:
+        return pt.ones_like( x, dtype=pt.bool )
+    return pt.abs( x - x.int() ) <= float_tol
